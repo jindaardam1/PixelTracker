ae9514c0880c96a8f067696e20be228f48d2c2f7
jindaardam1 <jindaardam1@gmail.com>
2023-12-15 20:46:25 +0100

    Added an identifier system to track the user

--- src/core/main.py
+++ src/core/main.py
@@ -10,32 +10,43 @@ init()
 app = Flask(__name__)
 
 
-@app.route('/tracker.png')
+@app.route('/pix.png')
 def tracker():
-    # Get the full path of the image
-    image_path = os.path.join(os.path.dirname(__file__), '../..', 'resources', 'pixel.png')
+    try:
+        # Get the full path of the image
+        image_path = os.path.join(os.path.dirname(__file__), '../..', 'resources', 'pixel.png')
 
-    # Try to get the public IP from the X-Forwarded-For header (in case behind a proxy)
-    user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+        # Safely get the client's IP address
+        user_ip = request.remote_addr
 
-    # Other additional information
-    user_agent = request.headers.get('User-Agent')
-    method = request.method
-    url = request.url
-    query_parameters = request.args
+        # Other additional information
+        user_agent = request.headers.get('User-Agent')
+        method = request.method
+        url = request.url
+        query_parameters = request.args
+        user_id = query_parameters.get('id')
 
-    # Log the email opening and user IP along with other information
-    print(Fore.GREEN + "-- A new email opening has been logged --" + Style.RESET_ALL)
-    print(f'\tEmail opened by {Fore.BLUE}{user_ip}{Style.RESET_ALL}')
-    print(f'\tUser-Agent: {Fore.BLUE}{user_agent}{Style.RESET_ALL}')
-    print(f'\tMethod: {method}, URL: {url}, Query Parameters: {query_parameters}')
-    print(Fore.RED + "\t", end="")
-    Logs.info_log_manager("New email opening logged in the database with IP " + user_ip)
-    print(Style.RESET_ALL, end="")
+        # Colored console log
+        print(Fore.GREEN + "-- A new email opening has been logged --" + Style.RESET_ALL)
+        print(f'\tEmail opened by {Fore.BLUE}{user_ip}{Style.RESET_ALL}')
+        print(f'\tUser-Agent: {Fore.BLUE}{user_agent}{Style.RESET_ALL}')
+        print(f'\tMethod: {method}, URL: {url}, Query Parameters: {query_parameters}')
+        print(f'\tUser ID: {Fore.YELLOW}{user_id}{Fore.RESET}')
+        print(Fore.RED + "\t", end="")
+        Logs.info_log_manager("New email opening logged in the database with IP " + user_ip)
+        print(Style.RESET_ALL, end="")
 
-    # Serve the 1-pixel generic image (transparent image)
-    return send_file(image_path)
+        # Return the 1-pixel image
+        return send_file(image_path)
+    except Exception as e:
+        # Handle the exception
+        print(f"An error occurred: {e}")
+        Logs.error_log_manager(e)
+        return "An error occurred", 500  # HTTP status code 500 indicates internal server error
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the application in debug mode only during development
+    app.run(debug=False)
+
+print(Fore.LIGHTGREEN_EX + "Server loaded successfully" + Fore.RESET)
